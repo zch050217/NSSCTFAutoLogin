012b38951c7bb26fa9fbfa16935e1649597989e6
yixinBC <yixinbc@foxmail.com>
2023-07-13 13:54:19 +0800

    update workflow

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # NSSCTFAutoLogin
-NSSCTF自动签到脚本，基于Github Action
+
+NSSCTF自动签到脚本，基于Github Action，添加了邮件通知和微信通知功能（可通过修改`.github/workflows/main.yml`文件关闭）。
+
+使用[SendGrid](https://sendgrid.com/)的免费邮件额度发送邮件通知
+
+使用[Server酱](http://sc.ftqq.com/3.version)的免费微信通知
+
+## 使用方法
+
+1. Fork本仓库
+2. 转到`Actions`，点击`I understand my workflows, go ahead and enable them`，启用Actions
+3. 在Fork后的仓库中，依次点击`Settings`->`Security`->`Secrets and variables`->`Actions`->`New repository secret`，添加以下secret：
+   - `NSSCTF_USERNAME` : NSSCTF的用户名
+   - `NSSCTF_PASSWORD` : NSSCTF的密码
+4. 添加以下secret（可选），若不想配置邮件通知与微信通知，请使用`#`注释掉`.github/workflows/main.yml`文件中的相关字段：
+   - `SENDGRID_API_KEY` : SendGrid的API Key，用于发送邮件通知
+   - `SENDGRID_FROM_EMAIL` : SendGrid的发送邮箱地址
+   - `SENDGRID_TO_EMAIL` : 接收邮件通知的邮箱地址
+   - `FTQQ_SCKEY` : Server酱的SendKey，用于发送微信通知
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import html
 import os
-import httpx
+import re
+import requests
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
 USER_AGENT = (
@@ -8,48 +12,101 @@ USER_AGENT = (
 )
 
 
-async def login():
-    """使用账号密码登陆
-
-    Returns:
-        _type_: _description_
-    """
-    async with httpx.AsyncClient() as client:
-        resp = await client.post(
-            "https://www.nssctf.cn/api/user/login/",
-            headers={"User-Agent": USER_AGENT},
-            data={
-                "username": os.environ["NSSCTF_USERNAME"],
-                "password": os.environ["NSSCTF_PASSWORD"],
-            },
-        )
-        cookies = dict(resp.cookies)
-        cookies["token"] = resp.json()["data"]["token"]
-        return cookies
+def login():
+    """使用账号密码登陆 NSSCTF"""
+    resp = requests.post(
+        "https://www.nssctf.cn/api/user/login/",
+        headers={"User-Agent": USER_AGENT},
+        data={
+            "username": os.environ["NSSCTF_USERNAME"],
+            "password": os.environ["NSSCTF_PASSWORD"],
+        },
+    )
+    cookies = dict(resp.cookies)
+    cookies["token"] = resp.json()["data"]["token"]
+    return cookies
 
 
-async def signin(cookies):
-    async with httpx.AsyncClient(cookies=cookies) as client:
-        resp = await client.post(
-            "https://www.nssctf.cn/api/user/clockin/",
-            headers={"User-Agent": USER_AGENT},
-        )
-        return resp.json()["code"] == 200
+def signin(cookies):
+    """签到"""
+    resp = requests.post(
+        "https://www.nssctf.cn/api/user/clockin/",
+        headers={"User-Agent": USER_AGENT},
+        cookies=cookies,
+    )
+    return resp.json()["code"] == 200
 
 
-async def coin_num(cookies):
-    async with httpx.AsyncClient(cookies=cookies) as client:
-        resp = await client.post(
-            "https://www.nssctf.cn/api/user/info/opt/setting/",
-            headers={"User-Agent": USER_AGENT},
-        )
-        data = resp.json()
-        if data["code"] != 200:
-            return None
-        return data.get("data", {}).get("coin", None)
+def get_coin_num(cookies):
+    """获取金币数量"""
+    resp = requests.post(
+        "https://www.nssctf.cn/api/user/info/opt/setting/",
+        headers={"User-Agent": USER_AGENT},
+        cookies=cookies,
+    )
+    data = resp.json()
+    if data["code"] != 200:
+        return None
+    return data.get("data", {}).get("coin", None)
 
-async def main():
-    cookies = await login()
-    await signin(cookies)
-    coin_num = await coin_num(cookies)
-    
+
+def send_email(coin_num):
+    """发送邮件"""
+    if coin_num is not None:
+        plain_text_content = f"签到成功，当前金币数量为 {coin_num}"
+    else:
+        plain_text_content = "签到失败，请查看github action日志获取详细信息"
+    message = Mail(
+        from_email=os.environ["SENDGRID_FROM_EMAIL"],
+        to_emails=os.environ["SENDGRID_TO_EMAIL"],
+        subject="NSSCTF 签到结果",
+        plain_text_content=plain_text_content,
+    )
+    try:
+        sg = SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
+        sg.send(message)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def send_ftqq(coin_num):
+    """发送微信推送"""
+    if coin_num is not None:
+        content = f"签到成功，当前金币数量为 {coin_num}"
+    else:
+        content = "签到失败，请查看github action日志获取详细信息"
+    resp = requests.post(
+        f"https://sc.ftqq.com/{os.environ['FTQQ_SCKEY']}.send",
+        data={"text": "NSSCTF 签到结果", "desp": content},
+    )
+    return resp.json()["errno"] == 0
+
+
+def main():
+    cookies = login()
+    signin(cookies)
+    coin_num = get_coin_num(cookies)
+    if coin_num is None:
+        print("签到失败")
+    else:
+        print(f"签到成功，当前金币数量为 {coin_num}")
+        if (
+            os.environ.get("SENDGRID_API_KEY") is not None
+            and os.environ.get("SENDGRID_FROM_EMAIL") is not None
+            and os.environ.get("SENDGRID_TO_EMAIL") is not None
+        ):
+            if send_email(coin_num):
+                print("邮件发送成功")
+            else:
+                print("邮件发送失败")
+        if os.environ.get("FTQQ_SCKEY") is not None:
+            if send_ftqq(coin_num):
+                print("微信推送发送成功")
+            else:
+                print("微信推送发送失败")
+
+
+if __name__ == "__main__":
+    main()
